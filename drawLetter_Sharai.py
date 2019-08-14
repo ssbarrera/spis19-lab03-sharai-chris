b86054916039bc64cb7a8e3faba5db45393889f3
Sharai Barrera <spis19ab@its-cseb240-17.ucsd.edu>
2019-08-13 19:46:16 -0700

    my initial function

---
+++ drawLetter_Sharai.py
@@ -0,0 +1,18 @@
+#Write initial
+import turtle 
+def drawS(theTurtle):
+    theTurtle.forward(100)
+    theTurtle.left(90)
+    theTurtle.forward(100)
+    theTurtle.left(90)
+    theTurtle.forward(100)
+    theTurtle.right(90)
+    theTurtle.forward(100)
+    theTurtle.right(90)
+    theTurtle.forward(100)
+myTurtle = turtle.Turtle()  # Create a new Turtle object
+drawS(myTurtle)   # make the new Turtle draw the shape
+
+
+
+
